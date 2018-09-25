44042f2bacfee8b2e6b8ac6200d72a936a6b80f9
THEjoezack <me@joezack.com>
2018-09-25 07:42:01 -0400

    Now storing search settings in local storage

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import Results from './Results/Results.js'
 class App extends Component {
   constructor (props) {
     super(props)
+    debugger
+    const serializedSearchConfig = window.localStorage.getItem('searchConfig')
+    const searchConfig = serializedSearchConfig ? JSON.parse(serializedSearchConfig) : null
     this.state = {
-      searchConfig: null
+      searchConfig: searchConfig
     }
   }
+
   updateConfig (searchConfig) {
     this.setState({
       searchConfig: searchConfig
     })
+
+    window.localStorage.setItem('searchConfig', JSON.stringify(searchConfig))
   }
 
   render () {
