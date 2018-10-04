91fd50e93dcbb6386b0b97580dc908c609188e67
THEjoezack <me@joezack.com>
2018-10-04 13:38:24 -0400

    Fleshing out the request fields

--- src/ControlPanel/RequestSettings.js
+++ src/ControlPanel/RequestSettings.js
@@ -28,6 +28,14 @@ class RequestSettings extends Component {
     }
   }
 
+  validateFuzzySearch () {
+    // const queryTypeValue = window.document.getElementById('queryType').value
+    // if (queryTypeValue !== 'full') {
+    //   return
+    // }
+    // const fuzzySearchValue = window.document.getElementById('fuzzySearch').value
+  }
+
   reset () {
     this.refs.form.reset()
   }
@@ -37,33 +45,25 @@ class RequestSettings extends Component {
       <form onSubmit={this.handleSubmit.bind(this)} ref='form'>
         <div className='form-group'>
           <label htmlFor='search'>Query</label>
-          <input id='search' placeholder='Samples: Coding+Blocks, NOT Docker, React | VueJs, -JavaScript' className='form-control' aria-describedby='searchHelp' />
+          <input id='search' placeholder='Samples: Coding+Blocks, NOT Docker, React | VueJs, -JavaScript' className='form-control' aria-describedby='searchHelp' onBlur={this.validateFuzzySearch.bind(this)} />
           <small id='searchHelp' className='form-text text-muted'>Examples, including wild cards and regex <a href='https://docs.microsoft.com/en-us/rest/api/searchservice/simple-query-syntax-in-azure-search'>here</a></small>
         </div>
         <div className='form-group'>
-          <label htmlFor='maxResults'>Top</label>
-          <input type='number' id='maxResults' className='form-control' aria-describedby='maxResultsHelp' placeholder='Number of results to return, default is 50' />
-          <small id='maxResultsHelp' className='form-text text-muted'>Specifying a value greater than 1000 and there are more than 1000 results, only the first 1000 results will be returned, along with a link to the next page of results.</small>
+          <label htmlFor='top'>Top</label>
+          <input type='number' id='top' className='form-control' aria-describedby='topHelp' placeholder='Number of results to return, default is 50' />
+          <small id='topHelp' className='form-text text-muted'>Specifying a value greater than 1000 and there are more than 1000 results, only the first 1000 results will be returned, along with a link to the next page of results.</small>
         </div>
         <div className='form-group'>
           <label htmlFor='queryType'>Query Type</label>
-          <select id='apiVersion' className='form-control' aria-describedby='queryTypeHelp' required value={this.state.queryType} onChange={this.changeQueryType}>
+          <select id='queryType' className='form-control' aria-describedby='queryTypeHelp' required value={this.state.queryType} onChange={this.changeQueryType}>
             <option value='simple'>Simple</option>
             <option value='full'>Full</option>
           </select>
           <small id='queryTypeHelp' className='form-text text-muted'>More details <a href='https://docs.microsoft.com/en-us/rest/api/searchservice/lucene-query-syntax-in-azure-search' title='More information on Lucene Query Syntax'>here</a></small>
         </div>
         <div className='form-group'>
-          <label htmlFor='highlighting'>Highlighting</label>
-          <select id='highlighting' className='form-control' aria-describedby='highlightingHelp'>
-            <option value=''>No</option>
-            <option>Yes</option>
-          </select>
-          <small id='highlightHelp' className='form-text text-muted'>Optionally can set <a href='https://docs.microsoft.com/en-us/rest/api/searchservice/search-documents' title='More information on highlighting'>pre/post tags</a></small>
-        </div>
-        <div className='form-group'>
           <label htmlFor='fuzzySearch'>Fuzzy Search (requires "full" query type)</label>
-          <select id='fuzzySearch' className='form-control' aria-describedby='fuzzySearchHelp' required disabled={this.state.queryType === 'simple'}>
+          <select id='fuzzySearch' className='form-control' aria-describedby='fuzzySearchHelp' disabled={this.state.queryType === 'simple'} onChange={this.validateFuzzySearch.bind()}>
             <option value=''>0 (no fuzzy search)</option>
             <option>1</option>
             <option>2</option>
@@ -82,7 +82,7 @@ class RequestSettings extends Component {
           <li>Term Boosting</li>
           <li>Proximity Search</li>
           <li>Paging</li>
-          <li>Highlight pre/post tag</li>
+          <li>Highlight pre/post tag, limit (requires schema)</li>
           <li>Sorting</li>
           <li><a href='https://docs.microsoft.com/en-us/rest/api/searchservice/index-operations' title='More about index operations'>Index Operations</a></li>
           <li><a href='https://docs.microsoft.com/en-us/rest/api/searchservice/document-operations' title='More about document operations'>Document Operations</a></li>
--- src/Models/SearchRequest.js
+++ src/Models/SearchRequest.js
@@ -32,7 +32,19 @@ class SearchRequest {
     let url = this.config.derivedProperties.baseSearchUrl
     let queryParams = []
 
-    if (requestConfig.search) {
+    if (requestConfig.top) {
+      queryParams.push('$top=' + window.encodeURI(requestConfig.top))
+    }
+
+    if (requestConfig.queryType === 'full') {
+      queryParams.push('queryType=' + window.encodeURI(requestConfig.queryType))
+    }
+
+    if (requestConfig.queryType === 'full' && requestConfig.fuzzySearch && requestConfig.search) {
+      const suffix = `~${requestConfig.fuzzySearch}`
+      const adjustedSearchTerm = requestConfig.search.split(' ').map(i => i + suffix).join(' ')
+      queryParams.push('search=' + window.encodeURI(adjustedSearchTerm))
+    } else if (requestConfig.search) {
       queryParams.push('search=' + window.encodeURI(requestConfig.search))
     }
 
